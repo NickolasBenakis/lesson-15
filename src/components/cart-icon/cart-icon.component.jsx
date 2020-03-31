7e97f03d2656175eba7a0aaf363f548d711b9436
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-31 16:40:58 +0300

    Add dynamic count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,18 +7,19 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
-  </div>
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+	<div className="cart-icon" onClick={toggleCartHidden}>
+		<ShoppingIcon className="shopping-icon" />
+		<span className="item-count">{itemCount > 99 ? 99 : itemCount}</span>
+	</div>
 );
 
 const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
+	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
