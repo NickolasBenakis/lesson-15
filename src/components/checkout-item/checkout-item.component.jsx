302f45bb30de7ab198ea35d070321114c60ba46a
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-31 20:35:15 +0300

    Add remove, add functionality checkout comp

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './checkout-item.styles.scss';
-const CheckoutItem = ({ item }) => {
+import { connect } from 'react-redux';
+import { removeItem, clearItem, addItem } from '../../redux/cart/cart.actions';
+const CheckoutItem = ({ item, clearCartItem, removeCartItem, addCartItem }) => {
 	const { imageUrl, name, quantity, price } = item;
 
 	return (
@@ -9,11 +11,27 @@ const CheckoutItem = ({ item }) => {
 				<img src={imageUrl} alt='item' />
 			</div>
 			<span className='name'>{name}</span>
-			<span className='quantity'>{quantity}</span>
+			<span className='quantity'>
+				<div className='arrow' onClick={() => removeCartItem(item)}>
+					&#10094;
+				</div>
+				<div className='value'>{quantity}</div>
+				<div className='arrow' onClick={() => addCartItem(item)}>
+					&#10095;
+				</div>
+			</span>
 			<span className='price'>{price}</span>
-			<div className='remove-button'>&#10005;</div>
+			<div className='remove-button' onClick={() => clearCartItem(item)}>
+				&#10005;
+			</div>
 		</div>
 	);
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+	clearCartItem: item => dispatch(clearItem(item)),
+	removeCartItem: item => dispatch(removeItem(item)),
+	addCartItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,29 @@ export const addItemToCart = (cartItems, currentItem) => {
 
 	return [...cartItems, { ...currentItem, quantity: 1 }];
 };
+export const removeItemFromCart = (cartItems, currentItemToRemove) => {
+	const existingItem = cartItems.find((item, index, self) => {
+		return item.id === currentItemToRemove.id;
+	});
+	if (existingItem && existingItem.quantity === 1) {
+		return cartItems.filter(cartItem => cartItem.id !== existingItem.id);
+	}
+
+	return cartItems.map(cartItem => {
+		return cartItem.id === currentItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem;
+	});
+};
+
+export const clearItemFromCart = (cartItems, currentItem) => {
+	const existingItem = cartItems.find((item, index, self) => {
+		return item.id === currentItem.id;
+	});
+	if (existingItem) {
+		return cartItems.filter(cartItem => {
+			return cartItem.id !== existingItem.id;
+		});
+	}
+	return cartItems;
+};
